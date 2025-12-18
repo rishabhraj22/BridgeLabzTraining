12fc35c6fce3a397c5a826fbe9d4a4d345a5d157
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-18 11:59:14 +0530

    [Rishabh]: added  age of harry

---
+++ gcr-codebase/programming-elements/Level_1/AgeOfHarry.java
@@ -0,0 +1,16 @@
+class AgeOfHarry{
+    public static void main(String[] args) {
+
+        // Birth year of Harry
+        int birthYear = 2000;
+
+        // Current year
+        int currentYear = 2024;
+
+        // Calculate age
+        int age = currentYear - birthYear;
+
+        // Display the result
+        System.out.println("Harry's age in 2024 is " + age);
+    }
+}
