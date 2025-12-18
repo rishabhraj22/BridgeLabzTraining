32e29d11b6f413b274bebc106b7049940be65b0b
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-18 17:11:03 +0530

    [Rishabh]: Added odd even number

---
+++ gcr-codebase/control-flow/Level_1/OddEvenNumber.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class OddEvenNumber {
+    public static void main(String[] args) {
+
+        // Declaring variable to store the number
+        int number;
+
+        Scanner input = new Scanner(System.in);
+        number = input.nextInt();
+
+        // Checking whether the number is a natural number
+        if (number > 0) {
+
+            // Using for loop to check odd and even numbers
+            for (int i = 1; i <= number; i++) {
+
+                // Checking if the number is even or odd
+                if (i % 2 == 0) {
+                    System.out.println(i + " is even number");
+                } else {
+                    System.out.println(i + " is odd number");
+                }
+            }
+
+        } else {
+
+            // Displaying message if number is not a natural number
+            System.out.println("Please enter a natural number");
+        }
+    }
+}
