b5e18fb8bba28a783eaf109d34c2b4141174e27a
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-22 12:39:08 +0530

    [Rishabh]: Added sub-string comparison

---
+++ gcr-codebase/strings/Level_1/SubstringComparison.java
@@ -0,0 +1,63 @@
+import java.util.Scanner;
+
+public class SubstringComparison {
+
+    // Method to create substring using charAt()
+    public static String createSubstringUsingCharAt(String text, int start, int end) {
+        String result = "";
+
+        for (int i = start; i < end; i++) {
+            result += text.charAt(i);
+        }
+
+        return result;
+    }
+
+    // Method to compare two strings using charAt()
+    public static boolean compareStringsUsingCharAt(String s1, String s2) {
+
+        if (s1.length() != s2.length()) {
+            return false;
+        }
+
+        for (int i = 0; i < s1.length(); i++) {
+            if (s1.charAt(i) != s2.charAt(i)) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        // Taking input
+        System.out.print("Enter the string: ");
+        String text = sc.next();
+
+        System.out.print("Enter start index: ");
+        int start = sc.nextInt();
+
+        System.out.print("Enter end index: ");
+        int end = sc.nextInt();
+
+        // Creating substring using charAt()
+        String manualSubstring = createSubstringUsingCharAt(text, start, end);
+
+        // Creating substring using built-in method
+        String builtInSubstring = text.substring(start, end);
+
+        // Comparing both substrings
+        boolean comparisonResult =
+                compareStringsUsingCharAt(manualSubstring, builtInSubstring);
+
+        // Displaying results
+        System.out.println("\nSubstring using charAt(): " + manualSubstring);
+        System.out.println("Substring using substring(): " + builtInSubstring);
+        System.out.println("Are both substrings equal? " + comparisonResult);
+
+        sc.close();
+    }
+}
