5756a52ee62d031e4b1e195e8c76be6c43eff482
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-27 16:34:50 +0530

    [Rishabh]: Added book class with attributes title, author, and price

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Book.java
@@ -0,0 +1,31 @@
+package com.constructor.level1;
+
+public class Book {
+
+    // Declaring instance variables
+    String title;
+    String author;
+    double price;
+
+    // Default constructor
+    public Book() {
+        title = "Unknown";
+        author = "Unknown";
+        price = 0.0;
+    }
+
+    // Parameterized constructor
+    public Book(String title, String author, double price) {
+        this.title = title;
+        this.author = author;
+        this.price = price;
+    }
+
+    // Method to display book details
+    public void display() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
+
