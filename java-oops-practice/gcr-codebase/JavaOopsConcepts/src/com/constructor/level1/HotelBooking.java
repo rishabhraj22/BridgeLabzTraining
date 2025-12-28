0d6b33ffd62fbb401d38a800a3c77da520d6283a
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-28 12:47:17 +0530

    [Rishabh]: Added hotelbooking class with attributes guestname, roomtype, and nights

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/HotelBooking.java
@@ -0,0 +1,38 @@
+package com.constructor.level1;
+
+public class HotelBooking {
+
+    // Declaring booking details
+    String guestName;
+    String roomType;
+    int nights;
+
+    // Default constructor
+    public HotelBooking() {
+        guestName = "Guest";
+        roomType = "Standard";
+        nights = 1;
+    }
+
+    // Parameterized constructor
+    public HotelBooking(String guestName, String roomType, int nights) {
+        this.guestName = guestName;
+        this.roomType = roomType;
+        this.nights = nights;
+    }
+
+    // Copy constructor
+    public HotelBooking(HotelBooking booking) {
+        this.guestName = booking.guestName;
+        this.roomType = booking.roomType;
+        this.nights = booking.nights;
+    }
+
+    // Method to display booking details
+    public void displayBooking() {
+        System.out.println("Guest Name: " + guestName);
+        System.out.println("Room Type: " + roomType);
+        System.out.println("Nights: " + nights);
+    }
+}
+
