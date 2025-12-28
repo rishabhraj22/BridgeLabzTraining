cc1e223a071a076e94718af18cd7433d41fc9fcb
rishabhraj22 <rajrishabh2209@gmail.com>
2025-12-28 12:58:22 +0530

    [Rishabh]: Added vehicle registration

---
+++ java-oops-practice/gcr-codebase/JavaOopsConcepts/src/com/constructor/level1/Vehicle.java
@@ -0,0 +1,30 @@
+package com.constructor.level1;
+
+public class Vehicle {
+
+    // Instance variables
+    String ownerName;
+    String vehicleType;
+
+    // Class variable for registration fee
+    static double registrationFee = 5000;
+
+    // Parameterized constructor
+    public Vehicle(String ownerName, String vehicleType) {
+        this.ownerName = ownerName;
+        this.vehicleType = vehicleType;
+    }
+
+    // Instance method to display vehicle details
+    public void displayVehicleDetails() {
+        System.out.println("Owner Name: " + ownerName);
+        System.out.println("Vehicle Type: " + vehicleType);
+        System.out.println("Registration Fee: " + registrationFee);
+    }
+
+    // Class method to update registration fee
+    public static void updateRegistrationFee(double newFee) {
+        registrationFee = newFee;
+    }
+}
+
