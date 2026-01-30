ab56c89c3074fe0cba9475aee4507b48c071dab4
rishabhraj22 <rajrishabh2209@gmail.com>
2026-01-30 14:26:41 +0530

    [Rishabh]: Added Convert a Java object (Car) into JSON format

---
+++ java-iostreams-practice/gcr-codebase/javaIOStreamsPractice/src/com/jsondata/car/Car.java
@@ -0,0 +1,12 @@
+package com.jsondata.car;
+public class Car {
+    public String brand;
+    public String model;
+    public int year;
+
+    public Car(String brand, String model, int year) {
+        this.brand = brand;
+        this.model = model;
+        this.year = year;
+    }
+}
